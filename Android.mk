6bde19f50e05b9dc600bd2d2aa216e81d7c766b0
XsMagical <XsMagical@team-nocturnal.net>
2013-10-09 17:06:44 -0400

    Fixed

--- Android.mk
+++ Android.mk
@@ -10,7 +10,7 @@ LOCAL_STATIC_JAVA_LIBRARIES := android-support-v13 android-support-v4
 
 LOCAL_SRC_FILES := $(call all-subdir-java-files)
 
-LOCAL_PACKAGE_NAME := ROMControl
+LOCAL_PACKAGE_NAME := NocturnalTweaks
 LOCAL_CERTIFICATE := platform
 include $(BUILD_PACKAGE)
 # Use the folloing include to make our test apk.
